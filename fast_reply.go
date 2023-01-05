adb1b94332cc2cf35acf863a2c472d229d395269
Dawid Rusnak <dawid@drcode.pl>
2023-01-05 18:38:44 +0100

    fix maximum FastReply code

--- fast_reply.go
+++ fast_reply.go
@@ -15,7 +15,7 @@ func (f FastReply) pass(w Writer) error {
 		return errors.New("fast reply code may be maximum of uint12")
 	}
 
-	if f.Code < MaxUint4 {
+	if f.Code <= MaxUint4 {
 		return w.WriteStandalone(newPacket(packetFastReplyLowBits|uint8(f.Code), 17).UUID(f.Id))
 	} else {
 		return w.WriteStandalone(newPacket(packetFastReplyHighBits|uint8(f.Code>>8), 18).Uint8(uint8(f.Code & 0x00ff)).UUID(f.Id))
